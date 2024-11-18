53d7ed57dab505927085cc42e8cf383fff606c0a
Hasan Berk Yilmaz <hasanberkyilmaz@gmail.com>
2024-11-18 11:29:37 +0300

    ucuncu todo

---
+++ ucuncu_ornerk/todo3.js
@@ -0,0 +1,9 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+
+
+addToDoButton.addEventListener('click', function(){
+    let paragraph = document.createElement('p');
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerHTML = inputText.value;
+})
